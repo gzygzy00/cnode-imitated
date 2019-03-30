698305e896eb2e980e0009b235611362ea16b19c
gzy <283211498@qq.com>
2019-03-30 15:36:05 +0800

    把过滤器filter转到main.js中

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -32,8 +32,6 @@
 </template>
 
 <script>
-  import Vue from 'vue'
-
   export default {
     name: "PostList",
     data: function () {
@@ -63,44 +61,6 @@
     }
   }
 
-
-  Vue.filter('tabFormatter', function (post) {
-    if (post.good === true) {
-      return '精华'
-    } else if (post.top === true) {
-      return '置顶'
-    } else if (post.tab === 'share') {
-      return '分享'
-    } else if (post.tab === 'ask') {
-      return '问答'
-    } else {
-      return '招聘'
-    }
-  })
-
-  Vue.filter('dateFormatter', function (str) {
-    if (!str) return ''
-    let date = new Date(str).getTime()
-    let now = new Date().getTime()
-    let time = (now - date) / 1000
-    if (time < 0) {
-      return ''
-    } else if (time < 30) {
-      return '刚刚'
-    } else if (time < 60) {
-      return Math.round(time) + '秒前'
-    } else if ((time / 60) < 60) {
-      return Math.round(time / 60) + '分钟前'
-    } else if ((time / (60 * 60)) < 24) {
-      return Math.round(time / (60 * 60)) + '小时前'
-    } else if ((time / (60 * 60 * 24)) < 31) {
-      return Math.round(time / (60 * 60 * 24)) + '天前'
-    } else if ((time / (60 * 60 * 24)) < 365) {
-      return Math.round(time / (60 * 60 * 24 * 31)) + '月前'
-    } else {
-      return Math.round(time / (60 * 60 * 24 * 365)) + '年前'
-    }
-  })
 </script>
 
 <style scoped>
